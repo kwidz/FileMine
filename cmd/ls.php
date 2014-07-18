2022262ccd743aedd30ace683557bdf05f99a29c
geoffrey glangine <geoffrey.glangine@gmail.com>
2014-07-18 21:19:47 +0200

    listage du dossier

---
+++ cmd/ls.php
@@ -0,0 +1,28 @@
+<?php 
+echo "passed !! ";
+if(isset($_GET["directory"])&& !empty($_GET["directory"])){
+	$nb_fichier = 0;
+	echo '<ul>';
+	if($dossier = opendir('/home/geoffrey')){
+		while(false !== ($fichier = readdir($dossier)))
+		{
+			if($fichier != '.'){
+				$nb_fichier++;
+				
+				echo"<div class='file'>
+						<span class='select'>
+							<input type='checkbox' />
+						</span>
+						<span class='name'>
+							$fichier
+						</span>
+					<span class='size'>
+						2 Go
+					</span>
+			</div>";
+			}
+		}
+
+	}
+	echo '</ul>';
+}
--- mine.js
+++ mine.js
@@ -1,7 +1,44 @@
 $(document).ready(function() {
-	$.getJSON("settings.json",function(result){
-
-		alert(result);
-
-	});
+	lister();
 });
+
+
+
+function getXMLHttpRequest() {
+	var xhr = null;
+	
+	if (window.XMLHttpRequest || window.ActiveXObject) {
+		if (window.ActiveXObject) {
+			try {
+				xhr = new ActiveXObject("Msxml2.XMLHTTP");
+			} catch(e) {
+				xhr = new ActiveXObject("Microsoft.XMLHTTP");
+			}
+		} else {
+			xhr = new XMLHttpRequest(); 
+		}
+	} else {
+		alert("Votre navigateur ne supporte pas l'objet XMLHTTPRequest...");
+		return null;
+	}
+	
+	return xhr;
+}
+
+
+
+function lister(){
+
+	var xhr = getXMLHttpRequest(); 
+	xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && (xhr.status == 200 || xhr.status == 0)) {
+        		
+        		document.getElementById("body").innerHTML= xhr.responseText; // Données textuelles récupérées
+        		
+        }
+	};
+	xhr.open("GET", "cmd/ls.php?directory="+"~", true);
+	xhr.send();
+
+}
+
