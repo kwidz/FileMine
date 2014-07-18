2f580b00adca4f0a2f10d84bb0f5460d389c0815
geoffrey glangine <geoffrey.glangine@gmail.com>
2014-07-18 22:31:15 +0200

    finalisation du listage

--- cmd/ls.php
+++ cmd/ls.php
@@ -1,5 +1,6 @@
 <?php
 if(isset($_GET["directory"])&& !empty($_GET["directory"])){
+	$directory=$_GET["directory"];
 	$nb_fichier = 0;
 	if($dossier = opendir($_GET["directory"])){
 		while(false !== ($fichier = readdir($dossier)))
@@ -8,7 +9,8 @@ if(isset($_GET["directory"])&& !empty($_GET["directory"])){
 				$nb_fichier++;
 
 
-				if (is_dir($fichier)) {
+				if (is_dir($directory."/".$fichier)) {
+
 					echo"<div class='folder'>
 					<span class='select'>
 					<input type='checkbox' />
@@ -21,7 +23,7 @@ if(isset($_GET["directory"])&& !empty($_GET["directory"])){
 					</span>
 					</div>";	
 				}else{
-					
+
 
 					echo"<div class='file'>
 					<span class='select'>
