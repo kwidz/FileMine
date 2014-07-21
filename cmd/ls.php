54bac0b7c1be14b12be2484e76b2cfa8f7fae1f9
Jean Mercadier <jeanmercadier@live.com>
2014-07-21 17:56:53 +0200

    fonctionnement des dossiers

--- cmd/ls.php
+++ cmd/ls.php
@@ -5,20 +5,27 @@ header("Content-Type: text/plain");
 $err    = "";
 $result = "";
 if(isset($_GET["dir"]) && !empty($_GET["dir"])){
-	$dir=$_GET["dir"];
-	// $settings = file_get_contents("../settings.json");
-	// $settings = json_decode($settings);
+	$dir      = $_GET["dir"];
+	$settings = file_get_contents("../settings.json");
+	$settings = (array)json_decode($settings);
+	if(substr($settings["paths"][0],-1)=="/" && $dir[0]=="/"){
+		$dir = substr($dir, 1);
+	}
+	else if(substr($settings["paths"][0],-1)!="/" && $dir[0]!="/"){
+		$settings["paths"][0] .= "/";
+	}
+	$dir = $settings["paths"][0] . $dir;
 	$nb_fichier = 0;
-	if($dossier = @opendir($_GET["dir"])){
+	if($dossier = @opendir($dir)){
 		$sep = "";
-		while(false !== ($fichier = readdir($dossier)))
-		{
+		while(false !== ($fichier = readdir($dossier))){
 			if($fichier != '.'){
 				$nb_fichier += 1;
 				if (is_dir($dir."/".$fichier)) {
-					$type = "file";
-				}else{
 					$type = "folder";
+				}
+				else{
+					$type = "file";
 				}
 				$result .= $sep . "{\"name\":\"$fichier\",\"type\":\"$type\",\"size\":\"-\"}\n";
 				$sep     = ",";
@@ -26,7 +33,7 @@ if(isset($_GET["dir"]) && !empty($_GET["dir"])){
 		}
 	}
 	else {
-		$err .= "Failed to open directory.";
+		$err .= "Failed to open " . $_GET["dir"];
 	}
 }
 else {
--- mine.js
+++ mine.js
@@ -1,4 +1,26 @@
 $(document).ready(function() {
+	$path = "/"; // NE PAS CHANGER pour editer le chemin allez dans le fichier de configuration
+	var updatepath = (function () {
+		$("#path").empty();
+		$splitted = $path.split("/");
+		$.each($splitted,function ($i,$dir){
+			if($i!=0) {
+				$("<span>/</span>").appendTo("#path");
+				$lien = $("<a>" + $dir + "</a>");
+				$lien.click(function(){
+					$toopen = ""
+					for($j=0;$j<=$i;$j++){
+						$sep = "/";
+						if($j==0 || $j==1) $sep = "";
+						$toopen += $sep;
+						$toopen += $splitted[$j];
+					}
+					lister.call(this,$toopen);
+				});
+				$lien.appendTo("#path");
+			};
+		});
+	});
 	var lister = (function($dir){
 		$.get( "cmd/ls.php", { dir: $dir} ).done(function($data) {
 			if ($data) {
@@ -7,18 +29,27 @@ $(document).ready(function() {
 					alert($data['err']);
 				}
 				else {
-					$("#path").html($data['path']);
+					$path = $data['path'];
+					updatepath.call(this);
 					$("#file_container").empty();
 					$.each($data['list'],function($i,$file){
 						$ligne = $("<div class='" + $file['type'] + "'></div>");
 						$("<span class='select'><input type='checkbox' /></span>").appendTo($ligne);
 						$("<span class='name'>" + $file['name'] + "</span>").appendTo($ligne);
 						$("<span class='size'>" + $file['size'] + "</span>").appendTo($ligne);
+						$ligne.click(function(){
+							if ($file['type']=="folder") cd.call(this,$file['name']);
+						});
 						$ligne.appendTo("#file_container");
 					});
 				}
 			};
 		});
 	});
-	lister.call(this,"/");
+	var cd = (function ($folder) {
+		$sep = "/";
+		if ($path == "/") $sep = "";
+		lister.call(this,$path + $sep + $folder);
+	});
+	lister.call(this,$path);
 });
