49805d77bb6b53a17e4a7912f2d9cb717f0d834c
geoffrey glangine <geoffrey.glangine@gmail.com>
2015-01-19 20:13:10 -0500

    bug

--- mine.js
+++ mine.js
@@ -148,12 +148,6 @@ $(document).ready(function() {
 					url=$file.copyURLFunction($chemin + "/" + $name);
 					var reg=new RegExp(" ", "g");
 					url=url.replace(reg,"%20");
-					var reg=new RegExp("?", "g");
-					url=url.replace(reg,"\\?");
-					var reg=new RegExp("=", "g");
-					url=url.replace(reg,"\\=");
-					var reg=new RegExp("&", "g");
-					url=url.replace(reg,"\\&");
 					clientNew.setText(window.location.host+"/"+url);
 				});
 
